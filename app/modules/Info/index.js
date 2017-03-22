a8b2f6948cb699597225ecd0a6a9f6a9eab3f897
Mike Hedman <mikehedman@gmail.com>
2017-03-21 21:38:19 -0700

    Cleaning up info page

--- app/modules/Info/index.js
+++ app/modules/Info/index.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
 import Header from '../../shared/components/Header';
-import Awake from '../../shared/Awake';
-import Sounds from '../../shared/Sounds';
-import Button from 'react-bootstrap/lib/Button';
 
 export default class Info extends Component {
-  toggleAwake() {
-    Awake.stayAwake();
-    Sounds.enable();
-    setTimeout(function() {Sounds.play()}, 3000);
-  }
-
   render() {
     return (
       <div>
         <Header leftLink="/" leftText="Settings"/>
-
-        <div>This is the info page</div>
-        <button onClick={this.toggleAwake}>Toggle awake</button>
-        <Button bsStyle="danger">Danger</Button>
+        <p>Planks is an exercise timing app. It's not a doctor, coach, or a whip. All it does is help keep time. The plank exercise can be strenuous - DO NOT attempt to do the plank exercise unless you have been cleared by your physician for rigorous exercise.</p>
+        <p>Additionally, using proper form when planking is critical. If you are not 100% sure of how to do the exercise, find a knowledgeable coach who can instruct you.</p>
+        <p>When you are first starting out, only attempt short durations.</p>
       </div>
     );
   }
