a57f54f112328b9c8d1366baf30c8facecd06bf7
Mike Hedman <mikehedman@gmail.com>
2017-03-21 21:38:03 -0700

    Adding meta tags and apple home page icon

--- app/modules/index.js
+++ app/modules/index.js
@@ -3,11 +3,13 @@ import React, { Component, cloneElement } from 'react';
 import { useStrict } from 'mobx';
 
 import { Provider } from 'mobx-react';
-import DevTools from 'mobx-react-devtools';
-
+//import DevTools from 'mobx-react-devtools';
 
 import styles from './sharedStyles.css';
 import 'bootstrap/dist/css/bootstrap.css';
+
+//import it just so that webpack includes it
+import apple_touch_icon from '!!file?name=apple-touch-icon.png!../apple-touch-icon.png';
 
 
 import { store as settingsStore } from './Settings';
--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,17 @@ module.exports = getConfig({
   in: 'app/client.js',
   out: 'public',
   clearBeforeBuild: true,
-  devtool: 'eval-source-map'
+  devtool: 'eval-source-map',
+  html: function (context) {
+    let data = {
+      title: 'Planks',
+      metaTags: {
+        "apple-mobile-web-app-title": "Planks3",
+        "application-name": "Planks3"
+      }
+    };
+    return {
+      'index.html': context.defaultTemplate(data)
+    }
+  }
 })
